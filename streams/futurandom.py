0624eb314e69eaf6930761e2bec2f463c4cb4712
Nina Margrethe Smørsgård <nina.m.smorsgard@gmail.com>
2014-12-04 10:00:11 +0100

    Initial: Added Futurama and South Park randoms

---
+++ streams/futurandom.py
@@ -0,0 +1,10 @@
+from random import randint
+import webbrowser
+
+show = "futurama"
+seasonsdict = {1:9, 2:20, 3:15, 4:12, 5:16, 6:26, 7:26}
+s = randint(1, len(seasonsdict))
+e = randint(1, seasonsdict[s])
+
+url = "http://watchonlinefree.tv/tv/"+show+"/season"+str(s)+"/episode"+str(e)
+webbrowser.open(url)
---
+++ streams/soutparandom.py
@@ -0,0 +1,10 @@
+from random import randint
+import webbrowser
+
+show = "south-park"
+seasonsdict = {1:13, 2:18, 3:17, 4:17, 5:14, 6:17, 7:15, 8:14, 9:14, 10:14, 11:14, 12:14, 13:14, 14:14, 15:14, 16:14, 17:10, 18:9}
+s = randint(1,len(seasonsdict))
+e = randint(1, seasonsdict[s])
+
+url = "http://watchonlinefree.tv/tv/"+show+"/season"+str(s)+"/episode"+str(e)
+webbrowser.open(url)
